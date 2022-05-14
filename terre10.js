4b3db0ae6c067bad232a16735c1815452968c1b7
RoMTHoS <romain.matheos.31@gmail.com>
2022-05-14 20:41:52 +0200

    Script qui trouve la racine carré d'un nombre

--- terre10.js
+++ terre10.js
@@ -1,58 +1,66 @@
 #! node
 
-const arg = process.argv[2]
-let start = arg
+const arg = process.argv[2];
+let start = arg;
 let int = [];
-let car; let min; let max; let mil;
-let stop = 0; let val = []; let y = 0;
+let car;
+let min;
+let max;
+let mil;
+let stop = 0;
+let val = [];
+let y = 0;
 
 if (arg % 1 !== 0) {
-  console.log("Veuillez indiquer un chiffre entier en argument")
-  return
+  console.log("Veuillez indiquer un chiffre entier en argument");
+  return;
 }
 
 //Boucle qui détermine un interval assez proche de la racine carrée
 while (int.length < 2) {
-  start = Math.round(start / 2)
-  if ((start * start) > arg) {        //Détermine la valeur haute de l'interval
-    int.shift()
-    int.push(start)
-  } else if ((start * start) < arg) { //Détermine la valeur basse de l'interval
-    int.unshift(start)
+  start = Math.round(start / 2);
+  if (start * start > arg) {
+    //Détermine la valeur haute de l'interval
+    int.shift();
+    int.push(start);
+  } else if (start * start < arg) {
+    //Détermine la valeur basse de l'interval
+    int.unshift(start);
   } else {
-    console.log("La racine carré de " + arg + " est " + start)
-    break
+    console.log("La racine carré de " + arg + " est " + start);
+    return;
   }
 }
-console.log(int)
 
 min = int[0];
 max = int[1];
 mil = Math.round((max - min) / 2);
-car = min + mil
+car = min + mil;
 
 // Boucle qui détermine le carré du nombre en mettant au carré le millieu de l'intervale
 if (car * car == arg) {
-    console.log("La racine carré de " + arg + " est " + car)
-    return
+  console.log("La racine carré de " + arg + " est " + car);
+  return;
 }
 
 while (stop < 100) {
-  if (car * car > arg) {            //Ajoute au millieu la moitié de la différence
-    mil /= 2                        //entre le max et le min de l'interval
-    car -= Math.round(mil)
-    y++
-    val[y] = car
-  } else if (car * car < arg) {     //Soustrait au miilieu la moitié de la différence
-    mil /= 2                         //entre le max et min de l'interval
-    car += Math.round(mil)
-    y++
-    val[y] = car
+  if (car * car > arg) {
+    //Ajoute au millieu la moitié de la différence
+    mil /= 2; //entre le max et le min de l'interval
+    car -= Math.round(mil);
+    y++;
+    val[y] = car;
+  } else if (car * car < arg) {
+    //Soustrait au miilieu la moitié de la différence
+    mil /= 2; //entre le max et min de l'interval
+    car += Math.round(mil);
+    y++;
+    val[y] = car;
   } else if (car * car == arg) {
-    console.log("La racine carré de " + arg + " est " + car)
-    return
+    console.log("La racine carré de " + arg + " est " + car);
+    return;
   }
-  stop++
+  stop++;
 }
 
-console.log(arg + " n'a pas de racine carrée")
+console.log(arg + " n'a pas de racine carrée");
